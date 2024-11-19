1e99e9836dcc0692c9e5fb465bb55546c936ef97
Avinashvmetre20 <avinashvmetre20@gmail.com>
2024-11-19 20:49:54 +0530

    added even odd number code

--- Index.js
+++ Index.js
@@ -8,9 +8,3 @@ const evens = numbers.filter(n => n % 2 === 0);
 const odds = numbers.filter(n => n % 2 !== 0);
 console.log("Even numbers:", evens);
 console.log("Odd numbers:", odds);
-
-// Print a triangle pattern
-const rows = 5;
-for (let i = 1; i <= rows; i++) {
-    console.log('*'.repeat(i));
-}
